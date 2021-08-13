f29bea00dcf5c468f2ee8f7902b5d4db6927b694
David Smith <Solaris17@users.noreply.github.com>
2021-08-13 19:16:26 -0400

    Update 20.04.sh

--- 20.04.sh
+++ 20.04.sh
@@ -10,3 +10,8 @@ mv 50unattended-upgrades 50unattended-upgrades.bk > /dev/null 2>&1
 wget teamdotexe.org/downloads/20auto-upgrades -O /etc/apt/apt.conf.d/20auto-upgrades > /dev/null 2>&1
 wget teamdotexe.org/downloads/50unattended-upgrades -O /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null 2>&1
 /etc/init.d/unattended-upgrades restart > /dev/null 2>&1
+
+echo
+echo Auto Updates Configured
+echo
+exit
